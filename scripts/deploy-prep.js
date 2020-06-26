a25ba801649c9974677664da26446007969f383a
Bill Warren <w.s.warren12@gmail.com>
2020-06-25 21:16:27 -0400

    few clean ups to inital commits, mostly in deploy scripts

--- scripts/deploy-prep.js
+++ scripts/deploy-prep.js
@@ -2,23 +2,13 @@ const fs = require("fs");
 const yaml = require("js-yaml");
 
 const config = {
-  kovan: {
-    v1FactoryAddress: "0x0C60Cd59f42093c7489BA68BAA4d7A01f2468153",
-    v1FactoryStartBlock: 14980875,
-    v2FactoryAddress: "0xB47778d3BcCBf5e39dEC075CA5F185fc20567b1e",
-    v2FactoryStartBlock: 16845360,
-  },
   rinkeby: {
-    v1FactoryAddress: "0x610247467d0dfA8B477ad7Dd1644e86CB2a79F8F",
-    v1FactoryStartBlock: 6494343,
-    v2FactoryAddress: "0x763b61A62EF076ad960E1d513713B2aeD7C1b88e",
-    v2FactoryStartBlock: 6494329,
+    v2FactoryAddress: "", 
+    v2FactoryStartBlock: 6731901,
   },
   mainnet: {
-    v1FactoryAddress: "0x2840d12d926cc686217bb42b80b662c7d72ee787",
-    v1FactoryStartBlock: 8625240,
-    v2FactoryAddress: "0x1782a13f176e84Be200842Ade79daAA0B09F0418",
-    v2FactoryStartBlock: 9484660,
+    v2FactoryAddress: "",
+    v2FactoryStartBlock: 10338274,
   },
 };
 
@@ -28,10 +18,6 @@ try {
   let fileContents = fs.readFileSync("./subgraph-template.yaml", "utf8");
   let data = yaml.safeLoad(fileContents);
 
-  data.dataSources[0].network = network;
-  data.dataSources[0].source.address = config[network].v1FactoryAddress;
-  data.dataSources[0].source.startBlock = config[network].v1FactoryStartBlock;
-
   data.dataSources[1].network = network;
   data.dataSources[1].source.address = config[network].v2FactoryAddress;
   data.dataSources[1].source.startBlock = config[network].v2FactoryStartBlock;
@@ -40,8 +26,7 @@ try {
   data.templates[1].network = network;
 
   if (network !== "mainnet") {
-    data.dataSources.splice(2, 4);
-    // data.dataSources.splice(3);
+    data.dataSources.splice(4);
   }
 
   let yamlStr = yaml.safeDump(data);
